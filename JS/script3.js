ea2e51ee4f33987b3fab2fb71edad913e36371d3
Aibek Shergazy <aibek.shergazy.kk@gmail.com>
2022-05-21 10:41:13 +0600

    add script3

---
+++ JS/script3.js
@@ -0,0 +1,9 @@
+/* Напишите несколько строк кода, 
+выводящих на экран ваше имя и почтовый адрес*/
+
+let userName = "Aibek";
+
+document.write(userName);
+
+let userAddress = "Bishkek, Goodstreet 32";
+document.write("<br>" + userAddress);
