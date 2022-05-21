8d7f073a2562439aff1a9859f8d66c0d5342d126
Aibek Shergazy <aibek.shergazy.kk@gmail.com>
2022-05-21 09:49:42 +0600

    New script

---
+++ JS/script2.js
@@ -0,0 +1,9 @@
+/* Простые сообщения: сохраните сообщение в переменной и выведите это сообщение. Затем замените значение переменной другим сообщением и выведите новое сообщение */
+
+let b = "It's my first script. Script two ";
+
+document.write(b + "<br>");
+
+b = "I changed text. This is a new text.";
+
+document.write(b);
